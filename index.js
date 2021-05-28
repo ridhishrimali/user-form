24deeca2005c920d3d3ee9d2f29669d0de68b692
rajshri <rajshri@nurturelabs.co>
2021-05-28 18:31:33 +0530

    modal changes

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-let selectedFileName = "";
+const signleFileElement = document.getElementById("singleFile");
+const signleFileNameElement = document.getElementById("singleFileName");
 
-const fileInputElement = document.getElementById("file");
-
-fileInputElement.addEventListener("change", (event) => {
+// for sigle file selection
+signleFileElement.addEventListener("change", (event) => {
   const fileList = event.target.files;
   console.log(fileList);
-  selectedFileName = fileList[0].name;
+  signleFileNameElement.innerText = fileList[0].name;
 });
 
-function submitHandler() {
-  alert(`Selected file name is ${selectedFileName}`);
-}
+const multiFileElement = document.getElementById("multiFile");
+const multiFileNameElement = document.getElementById("multiFileName");
+
+// for multi file selection
+multiFileElement.addEventListener("change", (event) => {
+  const fileList = event.target.files;
+  console.log(fileList);
+  for (let i = 0; i < fileList.length; i++) {
+    let p = document.createElement("p");
+    const fileList = event.target.files;
+    p.innerText = fileList[i].name;
+    multiFileNameElement.appendChild(p);
+  }
+});
